cf141a4d411799d213afe9f911399c5eba1f5885
Lalit Adithya <lalitadithya5071@gmail.com>
2017-04-15 21:41:58 +0530

    Add project files.

---
+++ LbtcWebsite/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LbtcWebsite
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
